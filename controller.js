dd4201cb694352288ff3710237d7f68163038f21
Uegdx <jhezico@gmail.com>
2022-12-01 12:21:34 -0800

    Berhasil menampilkan semuda data dari database

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
   respon.ok("Aplikasi Rest API berjalan", res)
 };
+
+// menampilkan semuda data mahasiswa
+exports.tampilSemuaDataMahasiswa = function (req, res) {
+  connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
+    if (error) {
+      connection.log(error)
+    } else {
+      respon.ok(rows, res)
+    }
+  });
+};
